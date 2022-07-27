93a31f8ce6e1a960e228458c4a2a796ef3040c63
hgseo16 <seohyounggyoo@gmail.com>
2022-07-27 22:57:17 +0900

    testing coo coc v2

--- backtrader_backtest/single_stock_test.py
+++ backtrader_backtest/single_stock_test.py
@@ -90,43 +90,14 @@ class OverNight(bt.Strategy):
                  (trade.pnl, trade.pnlcomm))
 
     def next(self):
-        # Simply log the closing price of the series from the reference
-        self.log('Close, %.2f' % self.dataclose[0])
-
-        # Check if an order is pending ... if yes, we cannot send a 2nd one
-        if self.order:
-            return
-
-        # Check if we are in the market
-        if not self.position:
-
-            # Not yet ... we MIGHT BUY if ...
-            if self.dataclose[0] < self.dataclose[-1]:
-                # current close less than previous close
-
-                if self.dataclose[-1] < self.dataclose[-2]:
-                    # previous close less than the previous close
-
-                    # BUY, BUY, BUY!!! (with default parameters)
-                    self.log('BUY CREATE, %.2f' % self.dataclose[0])
-
-                    # Keep track of the created order to avoid a 2nd order
-                    self.order = self.buy(coo=False, coc=True)
-
-        else:
-
-            # Already in the market ... we might sell
-            if len(self) >= (self.bar_executed + 5):
-                # SELL, SELL, SELL!!! (with all possible default parameters)
-                self.log('SELL CREATE, %.2f' % self.dataclose[0])
-
-                # Keep track of the created order to avoid a 2nd order
-                self.order = self.sell()
+        # order to sell at close price
+        self.order = self.sell(data=self.datas[0], coo=False, coc=True)
 
 
     def next_open(self):
-        # self.sell(coo=True, coc=False)
-        print('next_open')
+        # order to buy at open price
+        self.order = self.sell(data=self.datas[0], coo=False, coc=True)
+
 
 
 if __name__ == '__main__':
